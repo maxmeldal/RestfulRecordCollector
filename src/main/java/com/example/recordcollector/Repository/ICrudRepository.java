9d27435bd52db15b70d6a2e3628547c344481bf9
maxmeldal <maxmeldal@gmail.com>
2021-04-23 11:13:49 +0200

    commenting

--- src/main/java/com/example/recordcollector/Repository/ICrudRepository.java
+++ src/main/java/com/example/recordcollector/Repository/ICrudRepository.java
@@ -2,6 +2,9 @@ package com.example.recordcollector.Repository;
 
 import java.util.List;
 
+/*
+Standard Create, Read, Update and Delete interface, including the readAll method
+ */
 public interface ICrudRepository<T> {
     T create (T t);
     T read (int id);
